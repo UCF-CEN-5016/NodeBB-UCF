f43523bcc272911f8da00cd4cafffb5658c45d94
Brent Pappas <pappasbrent@gmail.com>
2024-01-23 12:54:40 -0500

    Translate pubsub.js to TypeScript
    - Translate `src/pubsub.js` to TypeScript in `src/pubsub.ts`.
    - Update `src/pubsub.js` with translated code.

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,60 +1,73 @@
-'use strict';
-
-const EventEmitter = require('events');
-const nconf = require('nconf');
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// eslint-disable-next-line import/no-import-module-exports
+const events_1 = __importDefault(require("events"));
+// eslint-disable-next-line import/no-import-module-exports
+const nconf_1 = __importDefault(require("nconf"));
+// eslint-disable-next-line import/no-import-module-exports
+const pubsub_1 = __importDefault(require("./database/redis/pubsub"));
 let real;
 let noCluster;
 let singleHost;
-
 function get() {
     if (real) {
         return real;
     }
-
     let pubsub;
-
-    if (!nconf.get('isCluster')) {
+    if (!nconf_1.default.get('isCluster')) {
         if (noCluster) {
             real = noCluster;
             return real;
         }
-        noCluster = new EventEmitter();
-        noCluster.publish = noCluster.emit.bind(noCluster);
+        noCluster = new events_1.default();
+        noCluster = Object.assign(noCluster, {
+            publish: noCluster.emit.bind(noCluster),
+        });
         pubsub = noCluster;
-    } else if (nconf.get('singleHostCluster')) {
+    }
+    else if (nconf_1.default.get('singleHostCluster')) {
         if (singleHost) {
             real = singleHost;
             return real;
         }
-        singleHost = new EventEmitter();
+        singleHost = new events_1.default();
         if (!process.send) {
-            singleHost.publish = singleHost.emit.bind(singleHost);
-        } else {
-            singleHost.publish = function (event, data) {
-                process.send({
-                    action: 'pubsub',
-                    event: event,
-                    data: data,
-                });
-            };
+            singleHost = Object.assign(singleHost, {
+                publish: singleHost.emit.bind(singleHost),
+            });
+        }
+        else {
+            singleHost = Object.assign(singleHost, {
+                publish: function (event, ...data) {
+                    return process.send({
+                        action: 'pubsub',
+                        event: event,
+                        data: data,
+                    });
+                },
+            });
             process.on('message', (message) => {
-                if (message && typeof message === 'object' && message.action === 'pubsub') {
+                if (message &&
+                    typeof message === 'object' &&
+                    message.action === 'pubsub') {
                     singleHost.emit(message.event, message.data);
                 }
             });
         }
         pubsub = singleHost;
-    } else if (nconf.get('redis')) {
-        pubsub = require('./database/redis/pubsub');
-    } else {
+    }
+    else if (nconf_1.default.get('redis')) {
+        pubsub = pubsub_1.default;
+    }
+    else {
         throw new Error('[[error:redis-required-for-pubsub]]');
     }
-
     real = pubsub;
     return pubsub;
 }
-
 module.exports = {
     publish: function (event, data) {
         get().publish(event, data);
---
+++ src/pubsub.ts
@@ -0,0 +1,96 @@
+// eslint-disable-next-line import/no-import-module-exports
+import EventEmitter from 'events';
+// eslint-disable-next-line import/no-import-module-exports
+import nconf from 'nconf';
+// eslint-disable-next-line import/no-import-module-exports
+import redisPubSub from './database/redis/pubsub';
+
+type emitType = typeof EventEmitter.prototype.emit;
+
+type PubSubEventEmitter = EventEmitter & { publish: emitType; };
+
+type PubSubEventEmitterOrEventEmitter = EventEmitter | PubSubEventEmitter;
+
+type ActionMessage = object & {
+    action: string;
+    event: string | symbol;
+    data: any;
+};
+
+let real: PubSubEventEmitterOrEventEmitter;
+let noCluster: PubSubEventEmitterOrEventEmitter;
+let singleHost: PubSubEventEmitterOrEventEmitter;
+
+
+function get(): PubSubEventEmitterOrEventEmitter {
+    if (real) {
+        return real;
+    }
+
+    let pubsub: PubSubEventEmitterOrEventEmitter;
+
+    if (!nconf.get('isCluster')) {
+        if (noCluster) {
+            real = noCluster;
+            return real;
+        }
+        noCluster = new EventEmitter();
+        noCluster = Object.assign(noCluster, {
+            publish: noCluster.emit.bind(noCluster) as emitType,
+        });
+        pubsub = noCluster;
+    } else if (nconf.get('singleHostCluster')) {
+        if (singleHost) {
+            real = singleHost;
+            return real;
+        }
+        singleHost = new EventEmitter();
+        if (!process.send) {
+            singleHost = Object.assign(singleHost, {
+                publish: singleHost.emit.bind(singleHost) as emitType,
+            });
+        } else {
+            singleHost = Object.assign(singleHost, {
+                publish: function (event: string | symbol, ...data: any[]): boolean {
+                    return process.send({
+                        action: 'pubsub',
+                        event: event,
+                        data: data,
+                    });
+                },
+            });
+            process.on('message', (message: null | ActionMessage) => {
+                if (
+                    message &&
+                    typeof message === 'object' &&
+                    message.action === 'pubsub'
+                ) {
+                    singleHost.emit(message.event, message.data);
+                }
+            });
+        }
+        pubsub = singleHost;
+    } else if (nconf.get('redis')) {
+        pubsub = redisPubSub as PubSubEventEmitter;
+    } else {
+        throw new Error('[[error:redis-required-for-pubsub]]');
+    }
+
+    real = pubsub;
+    return pubsub;
+}
+
+module.exports = {
+    publish: function (event: string | symbol, data: any): void {
+        (get() as PubSubEventEmitter).publish(event, data);
+    },
+    on: function (event: string | symbol, callback: (...args: any[]) => void): void {
+        get().on(event, callback);
+    },
+    removeAllListeners: function (event: string | symbol): void {
+        get().removeAllListeners(event);
+    },
+    reset: function (): void {
+        real = null;
+    },
+};
