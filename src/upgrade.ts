cc4574516287497ad893c80589c3c8dde05f7c4e
farbubby <farhan.mahbub@ucf.edu>
2024-01-27 14:29:03 -0500

    converted getAll function from js to ts

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -1,17 +1,16 @@
+/* eslint-disable import/no-import-module-exports */
+import path from 'path';
+import util from 'util';
+import semver from 'semver';
+import readline from 'readline';
+import winston from 'winston';
+import chalk from 'chalk';
 
-'use strict';
+import plugins from './plugins';
+import db from './database';
+import file from './file';
+import { paths } from './constants';
 
-const path = require('path');
-const util = require('util');
-const semver = require('semver');
-const readline = require('readline');
-const winston = require('winston');
-const chalk = require('chalk');
-
-const plugins = require('./plugins');
-const db = require('./database');
-const file = require('./file');
-const { paths } = require('./constants');
 /*
  * Need to write an upgrade script for NodeBB? Cool.
  *
@@ -21,30 +20,46 @@ const { paths } = require('./constants');
  * 3. Add your script under the "method" property
  */
 
-const Upgrade = module.exports;
+type Upgrade = {
+  getAll: () => Promise<string[]>; // 1 PART NOT DONE
+  appendPluginScripts: (files: string[]) => Promise<string[]>;
+  check: () => Promise<void>;
+  run: () => Promise<void>;
+  runParticular: (names: string[]) => Promise<void>;
+  process: (files: string[], skipCount: number) => Promise<void>;
+  incrementProgress: (value: number) => void;
+};
+
+const Upgrade: Upgrade = module.exports as Upgrade;
 
 Upgrade.getAll = async function () {
-    let files = await file.walk(path.join(__dirname, './upgrades'));
+    let files: string[] = (await file.walk(
+        path.join(__dirname, './upgrades')
+    )) as string[];
 
     // Sort the upgrade scripts based on version
-    files = files.filter(file => path.basename(file) !== 'TEMPLATE').sort((a, b) => {
-        const versionA = path.dirname(a).split(path.sep).pop();
-        const versionB = path.dirname(b).split(path.sep).pop();
-        const semverCompare = semver.compare(versionA, versionB);
-        if (semverCompare) {
-            return semverCompare;
-        }
-        const timestampA = require(a).timestamp;
-        const timestampB = require(b).timestamp;
-        return timestampA - timestampB;
-    });
+    files = files
+        .filter((file: string) => path.basename(file) !== 'TEMPLATE')
+        .sort((a: string, b: string) => {
+            const versionA = path.dirname(a).split(path.sep).pop();
+            const versionB = path.dirname(b).split(path.sep).pop();
+            const semverCompare = semver.compare(versionA, versionB);
+            if (semverCompare) {
+                return semverCompare;
+            }
+
+            // --------------- FIX THIS LATER ---------------------
+            const timestampA = require(a).timestamp;
+            const timestampB = require(b).timestamp;
+            return timestampA - timestampB;
+        });
 
     await Upgrade.appendPluginScripts(files);
 
     // check duplicates and error
     const seen = {};
-    const dupes = [];
-    files.forEach((file) => {
+    const dupes: string[] = [];
+    files.forEach((file: string) => {
         if (seen[file]) {
             dupes.push(file);
         } else {
@@ -52,7 +67,7 @@ Upgrade.getAll = async function () {
         }
     });
     if (dupes.length) {
-        winston.error(`Found duplicate upgrade scripts\n${dupes}`);
+        winston.error(`Found duplicate upgrade scripts\n${dupes.toString()}`);
         throw new Error('[[error:duplicate-upgrade-scripts]]');
     }
 
@@ -66,7 +81,10 @@ Upgrade.appendPluginScripts = async function (files) {
         const configPath = path.join(paths.nodeModules, plugin, 'plugin.json');
         try {
             const pluginConfig = require(configPath);
-            if (pluginConfig.hasOwnProperty('upgrades') && Array.isArray(pluginConfig.upgrades)) {
+            if (
+                pluginConfig.hasOwnProperty('upgrades') &&
+        Array.isArray(pluginConfig.upgrades)
+            ) {
                 pluginConfig.upgrades.forEach((script) => {
                     files.push(path.join(path.dirname(configPath), script));
                 });
@@ -84,7 +102,9 @@ Upgrade.check = async function () {
     // Throw 'schema-out-of-date' if not all upgrade scripts have run
     const files = await Upgrade.getAll();
     const executed = await db.getSortedSetRange('schemaLog', 0, -1);
-    const remainder = files.filter(name => !executed.includes(path.basename(name, '.js')));
+    const remainder = files.filter(
+        (name) => !executed.includes(path.basename(name, '.js'))
+    );
     if (remainder.length > 0) {
         throw new Error('schema-out-of-date');
     }
@@ -114,19 +134,25 @@ Upgrade.runParticular = async function (names) {
     console.log('\nParsing upgrade scripts... ');
     const files = await file.walk(path.join(__dirname, './upgrades'));
     await Upgrade.appendPluginScripts(files);
-    const upgrades = files.filter(file => names.includes(path.basename(file, '.js')));
+    const upgrades = files.filter((file) =>
+        names.includes(path.basename(file, '.js'))
+    );
     await Upgrade.process(upgrades, 0);
 };
 
 Upgrade.process = async function (files, skipCount) {
-    console.log(`${chalk.green('OK')} | ${chalk.cyan(`${files.length} script(s) found`)}${skipCount > 0 ? chalk.cyan(`, ${skipCount} skipped`) : ''}`);
+    console.log(
+        `${chalk.green('OK')} | ${chalk.cyan(`${files.length} script(s) found`)}${
+            skipCount > 0 ? chalk.cyan(`, ${skipCount} skipped`) : ''
+        }`
+    );
     const [schemaDate, schemaLogCount] = await Promise.all([
         db.get('schemaDate'),
         db.sortedSetCard('schemaLog'),
     ]);
 
     for (const file of files) {
-        /* eslint-disable no-await-in-loop */
+    /* eslint-disable no-await-in-loop */
         const scriptExport = require(file);
         const date = new Date(scriptExport.timestamp);
         const version = path.dirname(file).split('/').pop();
@@ -139,19 +165,41 @@ Upgrade.process = async function (files, skipCount) {
             date: date,
         };
 
-        process.stdout.write(`${chalk.white('  → ') + chalk.gray(`[${[date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate()].join('/')}] `) + scriptExport.name}...`);
+        process.stdout.write(
+            `${
+                chalk.white('  → ') +
+        chalk.gray(
+            `[${[
+                date.getUTCFullYear(),
+                date.getUTCMonth() + 1,
+                date.getUTCDate(),
+            ].join('/')}] `
+        ) +
+        scriptExport.name
+            }...`
+        );
 
         // For backwards compatibility, cross-reference with schemaDate (if found). If a script's date is older, skip it
-        if ((!schemaDate && !schemaLogCount) || (scriptExport.timestamp <= schemaDate && semver.lt(version, '1.5.0'))) {
+        if (
+            (!schemaDate && !schemaLogCount) ||
+      (scriptExport.timestamp <= schemaDate && semver.lt(version, '1.5.0'))
+        ) {
             process.stdout.write(chalk.grey(' skipped\n'));
 
-            await db.sortedSetAdd('schemaLog', Date.now(), path.basename(file, '.js'));
+            await db.sortedSetAdd(
+                'schemaLog',
+                Date.now(),
+                path.basename(file, '.js')
+            );
             // eslint-disable-next-line no-continue
             continue;
         }
 
         // Promisify method if necessary
-        if (scriptExport.method.constructor && scriptExport.method.constructor.name !== 'AsyncFunction') {
+        if (
+            scriptExport.method.constructor &&
+      scriptExport.method.constructor.name !== 'AsyncFunction'
+        ) {
             scriptExport.method = util.promisify(scriptExport.method);
         }
 
@@ -183,7 +231,7 @@ Upgrade.incrementProgress = function (value) {
 
     this.current += value || 1;
     this.counter += value || 1;
-    const step = (this.total ? Math.floor(this.total / 100) : 100);
+    const step = this.total ? Math.floor(this.total / 100) : 100;
 
     if (this.counter > step || this.current >= this.total) {
         this.counter -= step;
@@ -191,13 +239,17 @@ Upgrade.incrementProgress = function (value) {
         let filled = 0;
         let unfilled = 15;
         if (this.total) {
-            percentage = `${Math.floor((this.current / this.total) * 100)}%`;
+            percentage = Math.floor((this.current / this.total) * 100);
             filled = Math.floor((this.current / this.total) * 15);
             unfilled = Math.max(0, 15 - filled);
         }
 
         readline.cursorTo(process.stdout, 0);
-        process.stdout.write(`    [${filled ? new Array(filled).join('#') : ''}${new Array(unfilled).join(' ')}] (${this.current}/${this.total || '??'}) ${percentage} `);
+        process.stdout.write(
+            `    [${filled ? new Array(filled).join('#') : ''}${new Array(
+                unfilled
+            ).join(' ')}] (${this.current}/${this.total || '??'}) ${percentage} `
+        );
     }
 };
 
