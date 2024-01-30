947395959b492029c0a7c3de3397c5fe76d5d361
Jake Norris <jakenorris1124@gmail.com>
2024-01-30 17:38:17 -0500

    Finished translating the file

--- src/rewards/index.ts
+++ src/rewards/index.ts
@@ -3,42 +3,43 @@ import util = require('util');
 import db = require('../database');
 import plugins = require('../plugins');
 
-const rewards = module.exports;
-
-rewards.checkConditionAndRewardUser = async function (params) {
-    const { uid, condition, method } = params;
-    const isActive = await isConditionActive(condition);
-    if (!isActive) {
-        return;
-    }
-    const ids = await getIDsByCondition(condition);
-    let rewardData = await getRewardDataByIDs(ids);
-    rewardData = await filterCompletedRewards(uid, rewardData);
-    rewardData = rewardData.filter(Boolean);
-    if (!rewardData || !rewardData.length) {
-        return;
-    }
-    const eligible = await Promise.all(rewardData.map(reward => checkCondition(reward, method)));
-    const eligibleRewards = rewardData.filter((reward, index) => eligible[index]);
-    await giveRewards(uid, eligibleRewards);
+type Params = {
+    uid: string;
+    condition: string;
+    method: () => unknown;
 };
 
-async function isConditionActive(condition) {
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    return await db.isSetMember('conditions:active', condition);
+type Reward = {
+    id: string;
+    rid: string;
+    claimable: string;
+    conditional: string;
+    score: string;
+    value: number;
 }
 
-async function getIDsByCondition(condition) {
+type RewardsIndexModule = {
+    checkConditionAndRewardUser: (params: Params) => Promise<void>;
+};
+
+const rewards: RewardsIndexModule = module.exports as RewardsIndexModule;
+
+async function isConditionActive(condition: string): Promise<boolean> {
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    return await db.getSetMembers(`condition:${condition}:rewards`);
+    return await db.isSetMember('conditions:active', condition) as boolean;
 }
 
-async function filterCompletedRewards(uid, rewards) {
+async function getIDsByCondition(condition: string): Promise<string[]> {
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const data = await db.getSortedSetRangeByScoreWithScores(`uid:${uid}:rewards`, 0, -1, 1, '+inf');
+    return await db.getSetMembers(`condition:${condition}:rewards`) as string[];
+}
+
+async function filterCompletedRewards(uid: string, rewards: Reward[]): Promise<Reward[]> {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const data = await db.getSortedSetRangeByScoreWithScores(`uid:${uid}:rewards`, 0, -1, 1, '+inf') as Reward[];
     const userRewards = {};
 
     data.forEach((obj) => {
@@ -55,19 +56,19 @@ async function filterCompletedRewards(uid, rewards) {
     });
 }
 
-async function getRewardDataByIDs(ids) {
+async function getRewardDataByIDs(ids: string[]): Promise<Reward[]> {
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    return await db.getObjects(ids.map(id => `rewards:id:${id}`));
+    return await db.getObjects(ids.map(id => `rewards:id:${id}`)) as Reward[];
 }
 
-async function getRewardsByRewardData(rewards) {
+async function getRewardsByRewardData(rewards: Reward[]): Promise<Reward[]> {
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    return await db.getObjects(rewards.map(reward => `rewards:id:${reward.id}:rewards`));
+    return await db.getObjects(rewards.map(reward => `rewards:id:${reward.id}:rewards`)) as Reward[];
 }
 
-async function checkCondition(reward, method) {
+async function checkCondition(reward: Reward, method: () => unknown): Promise<boolean> {
     if (method.constructor && method.constructor.name !== 'AsyncFunction') {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -76,11 +77,11 @@ async function checkCondition(reward, method) {
     const value = await method();
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const bool = await plugins.hooks.fire(`filter:rewards.checkConditional:${reward.conditional}`, { left: value, right: reward.value });
+    const bool = await plugins.hooks.fire(`filter:rewards.checkConditional:${reward.conditional}`, { left: value, right: reward.value }) as boolean;
     return bool;
 }
 
-async function giveRewards(uid, rewards) {
+async function giveRewards(uid: string, rewards: Reward[]) {
     const rewardData = await getRewardsByRewardData(rewards);
     for (let i = 0; i < rewards.length; i++) {
         // The next line calls a function in a module that has not been updated to TS yet
@@ -93,4 +94,20 @@ async function giveRewards(uid, rewards) {
     }
 }
 
-require('../promisify')(rewards);
+rewards.checkConditionAndRewardUser = async function (params: Params): Promise<void> {
+    const { uid, condition, method } = params;
+    const isActive = await isConditionActive(condition);
+    if (!isActive) {
+        return;
+    }
+    const ids = await getIDsByCondition(condition);
+    let rewardData = await getRewardDataByIDs(ids);
+    rewardData = await filterCompletedRewards(uid, rewardData);
+    rewardData = rewardData.filter(Boolean);
+    if (!rewardData || !rewardData.length) {
+        return;
+    }
+    const eligible = await Promise.all(rewardData.map(reward => checkCondition(reward, method)));
+    const eligibleRewards = rewardData.filter((reward, index) => eligible[index]);
+    await giveRewards(uid, eligibleRewards);
+};
