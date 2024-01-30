41926c0ab7e4c4bb38726c999c594eef0db4d62f
Jake Norris <jakenorris1124@gmail.com>
2024-01-30 03:28:00 -0500

    Supress lint errors for functions that may not have been updated to TS yet

--- src/rewards/index.ts
+++ src/rewards/index.ts
@@ -24,14 +24,20 @@ rewards.checkConditionAndRewardUser = async function (params) {
 };
 
 async function isConditionActive(condition) {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return await db.isSetMember('conditions:active', condition);
 }
 
 async function getIDsByCondition(condition) {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return await db.getSetMembers(`condition:${condition}:rewards`);
 }
 
 async function filterCompletedRewards(uid, rewards) {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     const data = await db.getSortedSetRangeByScoreWithScores(`uid:${uid}:rewards`, 0, -1, 1, '+inf');
     const userRewards = {};
 
@@ -50,18 +56,26 @@ async function filterCompletedRewards(uid, rewards) {
 }
 
 async function getRewardDataByIDs(ids) {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return await db.getObjects(ids.map(id => `rewards:id:${id}`));
 }
 
 async function getRewardsByRewardData(rewards) {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return await db.getObjects(rewards.map(reward => `rewards:id:${reward.id}:rewards`));
 }
 
 async function checkCondition(reward, method) {
     if (method.constructor && method.constructor.name !== 'AsyncFunction') {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         method = util.promisify(method);
     }
     const value = await method();
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     const bool = await plugins.hooks.fire(`filter:rewards.checkConditional:${reward.conditional}`, { left: value, right: reward.value });
     return bool;
 }
@@ -69,8 +83,12 @@ async function checkCondition(reward, method) {
 async function giveRewards(uid, rewards) {
     const rewardData = await getRewardsByRewardData(rewards);
     for (let i = 0; i < rewards.length; i++) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         /* eslint-disable no-await-in-loop */
         await plugins.hooks.fire(`action:rewards.award:${rewards[i].rid}`, { uid: uid, reward: rewardData[i] });
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         await db.sortedSetIncrBy(`uid:${uid}:rewards`, 1, rewards[i].id);
     }
 }
