a3bb53413593467af3e32951e7543fd6052e68ff
Srikar-Konepudi <156713973+Srikar-Konepudi@users.noreply.github.com>
2024-01-31 02:39:35 -0500

    ts file updated

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,102 +8,117 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const categories = require('../categories');
-const events = require('../events');
-const user = require('../user');
-const groups = require('../groups');
-const privileges = require('../privileges');
-const categoriesAPI = module.exports;
-categoriesAPI.get = function (caller, data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const [userPrivileges, category] = yield Promise.all([
-            privileges.categories.get(data.cid, caller.uid),
-            categories.getCategoryData(data.cid),
-        ]);
-        if (!category || !userPrivileges.read) {
-            return null;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const categories_1 = __importDefault(require("../categories"));
+const events_1 = __importDefault(require("../events"));
+const user_1 = __importDefault(require("../user"));
+const groups_1 = __importDefault(require("../groups"));
+const privileges_1 = __importDefault(require("../privileges"));
+const categoriesAPI = {
+    get: function (caller, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const privCat = yield Promise.all([
+                privileges_1.default.categories.get(data.cid, caller.uid),
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                categories_1.default.getCategoryData(data.cid),
+            ]);
+            if (!privCat[1] || !privCat[0].read) {
+                return null;
+            }
+            return privCat[1];
+        });
+    },
+    create: function (caller, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const response = yield categories_1.default.create(data);
+            const categoryObjs = yield categories_1.default.getCategories([response.cid], caller.uid);
+            return categoryObjs[0];
+        });
+    },
+    update: function (caller, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!data) {
+                throw new Error('[[error:invalid-data]]');
+            }
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield categories_1.default.update(data);
+        });
+    },
+    delete: function (caller, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const name = yield categories_1.default.getCategoryField(data.cid, 'name');
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield categories_1.default.purge(data.cid, caller.uid);
+            yield events_1.default.log({
+                type: 'category-purge',
+                uid: caller.uid,
+                ip: caller.ip,
+                cid: data.cid,
+                name: name,
+            });
+        });
+    },
+    getPrivileges: (caller, cid) => __awaiter(void 0, void 0, void 0, function* () {
+        let responsePayload;
+        if (cid === 'admin') {
+            responsePayload = yield privileges_1.default.admin.list(caller.uid);
         }
-        return category;
-    });
-};
-categoriesAPI.create = function (caller, data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield categories.create(data);
-        const categoryObjs = yield categories.getCategories([response.cid], caller.uid);
-        return categoryObjs[0];
-    });
-};
-categoriesAPI.update = function (caller, data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (!data) {
+        else if (!parseInt(cid, 10)) {
+            responsePayload = yield privileges_1.default.global.list();
+        }
+        else {
+            responsePayload = yield privileges_1.default.categories.list(cid);
+        }
+        return responsePayload;
+    }),
+    setPrivilege: (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
+        const existsCheck = yield Promise.all([
+            user_1.default.exists(data.member),
+            groups_1.default.exists(data.member),
+        ]);
+        if (!existsCheck[0] && !existsCheck[1]) {
+            throw new Error('[[error:no-user-or-group]]');
+        }
+        const privs = Array.isArray(data.privilege) ? data.privilege : [data.privilege];
+        const type = data.set ? 'give' : 'rescind';
+        if (!privs.length) {
             throw new Error('[[error:invalid-data]]');
         }
-        yield categories.update(data);
-    });
-};
-categoriesAPI.delete = function (caller, data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const name = yield categories.getCategoryField(data.cid, 'name');
-        yield categories.purge(data.cid, caller.uid);
-        yield events.log({
-            type: 'category-purge',
+        if (parseInt(data.cid, 10) === 0) {
+            const adminPrivList = yield privileges_1.default.admin.getPrivilegeList();
+            const adminPrivs = privs.filter(priv => adminPrivList.includes(priv));
+            if (adminPrivs.length) {
+                yield privileges_1.default.admin[type](adminPrivs, data.member);
+            }
+            const globalPrivList = yield privileges_1.default.global.getPrivilegeList();
+            const globalPrivs = privs.filter(priv => globalPrivList.includes(priv));
+            if (globalPrivs.length) {
+                yield privileges_1.default.global[type](globalPrivs, data.member);
+            }
+        }
+        else {
+            const categoryPrivList = yield privileges_1.default.categories.getPrivilegeList();
+            const categoryPrivs = privs.filter(priv => categoryPrivList.includes(priv));
+            yield privileges_1.default.categories[type](categoryPrivs, data.cid, data.member);
+        }
+        yield events_1.default.log({
             uid: caller.uid,
+            type: 'privilege-change',
             ip: caller.ip,
+            privilege: data.privilege.toString(),
             cid: data.cid,
-            name: name,
+            action: data.set ? 'grant' : 'rescind',
+            target: data.member,
         });
-    });
+    }),
 };
-categoriesAPI.getPrivileges = (caller, cid) => __awaiter(void 0, void 0, void 0, function* () {
-    let responsePayload;
-    if (cid === 'admin') {
-        responsePayload = yield privileges.admin.list(caller.uid);
-    }
-    else if (!parseInt(cid, 10)) {
-        responsePayload = yield privileges.global.list();
-    }
-    else {
-        responsePayload = yield privileges.categories.list(cid);
-    }
-    return responsePayload;
-});
-categoriesAPI.setPrivilege = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
-    const [userExists, groupExists] = yield Promise.all([
-        user.exists(data.member),
-        groups.exists(data.member),
-    ]);
-    if (!userExists && !groupExists) {
-        throw new Error('[[error:no-user-or-group]]');
-    }
-    const privs = Array.isArray(data.privilege) ? data.privilege : [data.privilege];
-    const type = data.set ? 'give' : 'rescind';
-    if (!privs.length) {
-        throw new Error('[[error:invalid-data]]');
-    }
-    if (parseInt(data.cid, 10) === 0) {
-        const adminPrivList = yield privileges.admin.getPrivilegeList();
-        const adminPrivs = privs.filter(priv => adminPrivList.includes(priv));
-        if (adminPrivs.length) {
-            yield privileges.admin[type](adminPrivs, data.member);
-        }
-        const globalPrivList = yield privileges.global.getPrivilegeList();
-        const globalPrivs = privs.filter(priv => globalPrivList.includes(priv));
-        if (globalPrivs.length) {
-            yield privileges.global[type](globalPrivs, data.member);
-        }
-    }
-    else {
-        const categoryPrivList = yield privileges.categories.getPrivilegeList();
-        const categoryPrivs = privs.filter(priv => categoryPrivList.includes(priv));
-        yield privileges.categories[type](categoryPrivs, data.cid, data.member);
-    }
-    yield events.log({
-        uid: caller.uid,
-        type: 'privilege-change',
-        ip: caller.ip,
-        privilege: data.privilege.toString(),
-        cid: data.cid,
-        action: data.set ? 'grant' : 'rescind',
-        target: data.member,
-    });
-});
+module.exports = categoriesAPI;
--- src/api/categories.ts
+++ src/api/categories.ts
@@ -1,102 +1,137 @@
-'use strict';
+import categories from '../categories';
+import events from '../events';
+import user from '../user';
+import groups from '../groups';
+import privileges from '../privileges';
 
-const categories = require('../categories');
-const events = require('../events');
-const user = require('../user');
-const groups = require('../groups');
-const privileges = require('../privileges');
 
-const categoriesAPI = module.exports;
+import { UserObjectACP } from '../types';
 
-categoriesAPI.get = async function (caller, data) {
-    const [userPrivileges, category] = await Promise.all([
-        privileges.categories.get(data.cid, caller.uid),
-        categories.getCategoryData(data.cid),
-    ]);
-    if (!category || !userPrivileges.read) {
-        return null;
-    }
-
-    return category;
-};
-
-categoriesAPI.create = async function (caller, data) {
-    const response = await categories.create(data);
-    const categoryObjs = await categories.getCategories([response.cid], caller.uid);
-    return categoryObjs[0];
-};
-
-categoriesAPI.update = async function (caller, data) {
-    if (!data) {
-        throw new Error('[[error:invalid-data]]');
-    }
-    await categories.update(data);
-};
-
-categoriesAPI.delete = async function (caller, data) {
-    const name = await categories.getCategoryField(data.cid, 'name');
-    await categories.purge(data.cid, caller.uid);
-    await events.log({
-        type: 'category-purge',
-        uid: caller.uid,
-        ip: caller.ip,
-        cid: data.cid,
-        name: name,
-    });
-};
-
-categoriesAPI.getPrivileges = async (caller, cid) => {
-    let responsePayload;
-
-    if (cid === 'admin') {
-        responsePayload = await privileges.admin.list(caller.uid);
-    } else if (!parseInt(cid, 10)) {
-        responsePayload = await privileges.global.list();
-    } else {
-        responsePayload = await privileges.categories.list(cid);
-    }
-
-    return responsePayload;
-};
-
-categoriesAPI.setPrivilege = async (caller, data) => {
-    const [userExists, groupExists] = await Promise.all([
-        user.exists(data.member),
-        groups.exists(data.member),
-    ]);
-
-    if (!userExists && !groupExists) {
-        throw new Error('[[error:no-user-or-group]]');
-    }
-    const privs = Array.isArray(data.privilege) ? data.privilege : [data.privilege];
-    const type = data.set ? 'give' : 'rescind';
-    if (!privs.length) {
-        throw new Error('[[error:invalid-data]]');
-    }
-    if (parseInt(data.cid, 10) === 0) {
-        const adminPrivList = await privileges.admin.getPrivilegeList();
-        const adminPrivs = privs.filter(priv => adminPrivList.includes(priv));
-        if (adminPrivs.length) {
-            await privileges.admin[type](adminPrivs, data.member);
+type Data ={
+    cid: string;
+    privilege: string[];
+    member: string;
+    set: string;
+}
+type UserPriv ={
+    read: boolean;
+}
+type Response ={
+    cid: string;
+}
+interface CategoriesAPI {
+    get: (caller: UserObjectACP, data: Data) => Promise<unknown>;
+    create: (caller: UserObjectACP, data: Data) => Promise<unknown>;
+    update: (caller: UserObjectACP, data: Data) => Promise<unknown>;
+    delete: (caller: UserObjectACP, data: Data) => Promise<unknown>;
+    getPrivileges(caller: UserObjectACP, cid: string): Promise<unknown>;
+    setPrivilege(caller: UserObjectACP, data: Data): Promise<unknown>;
+}
+interface Priv {
+    includes(priv: string): Promise<unknown>;
+}
+const categoriesAPI: CategoriesAPI = {
+    get: async function (caller, data) {
+        const privCat: [UserPriv, boolean] = await Promise.all([
+            privileges.categories.get(data.cid, caller.uid),
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            categories.getCategoryData(data.cid),
+        ]) as [UserPriv, boolean];
+        if (!privCat[1] || !privCat[0].read) {
+            return null;
         }
-        const globalPrivList = await privileges.global.getPrivilegeList();
-        const globalPrivs = privs.filter(priv => globalPrivList.includes(priv));
-        if (globalPrivs.length) {
-            await privileges.global[type](globalPrivs, data.member);
-        }
-    } else {
-        const categoryPrivList = await privileges.categories.getPrivilegeList();
-        const categoryPrivs = privs.filter(priv => categoryPrivList.includes(priv));
-        await privileges.categories[type](categoryPrivs, data.cid, data.member);
-    }
 
-    await events.log({
-        uid: caller.uid,
-        type: 'privilege-change',
-        ip: caller.ip,
-        privilege: data.privilege.toString(),
-        cid: data.cid,
-        action: data.set ? 'grant' : 'rescind',
-        target: data.member,
-    });
+        return privCat[1];
+    },
+
+    create: async function (caller, data) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const response: Response = await categories.create(data) as Response;
+        const categoryObjs: number[] = await categories.getCategories([response.cid], caller.uid) as number[];
+        return categoryObjs[0];
+    },
+
+    update: async function (caller, data) {
+        if (!data) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await categories.update(data);
+    },
+
+    delete: async function (caller, data) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const name: string = await categories.getCategoryField(data.cid, 'name') as string;
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await categories.purge(data.cid, caller.uid);
+        await events.log({
+            type: 'category-purge',
+            uid: caller.uid,
+            ip: caller.ip,
+            cid: data.cid,
+            name: name,
+        });
+    },
+
+    getPrivileges: async (caller, cid) => {
+        let responsePayload: string;
+
+        if (cid === 'admin') {
+            responsePayload = await privileges.admin.list(caller.uid);
+        } else if (!parseInt(cid, 10)) {
+            responsePayload = await privileges.global.list();
+        } else {
+            responsePayload = await privileges.categories.list(cid);
+        }
+
+        return responsePayload;
+    },
+
+    setPrivilege: async (caller, data) => {
+        const existsCheck:[boolean, boolean] = await Promise.all([
+            user.exists(data.member),
+            groups.exists(data.member),
+        ]) as [boolean, boolean];
+
+        if (!existsCheck[0] && !existsCheck[1]) {
+            throw new Error('[[error:no-user-or-group]]');
+        }
+        const privs = Array.isArray(data.privilege) ? data.privilege : [data.privilege];
+        const type = data.set ? 'give' : 'rescind';
+        if (!privs.length) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        if (parseInt(data.cid, 10) === 0) {
+            const adminPrivList: Priv = await privileges.admin.getPrivilegeList() as Priv;
+            const adminPrivs = privs.filter(priv => adminPrivList.includes(priv));
+            if (adminPrivs.length) {
+                await privileges.admin[type](adminPrivs, data.member);
+            }
+            const globalPrivList: Priv = await privileges.global.getPrivilegeList() as Priv;
+            const globalPrivs = privs.filter(priv => globalPrivList.includes(priv));
+            if (globalPrivs.length) {
+                await privileges.global[type](globalPrivs, data.member);
+            }
+        } else {
+            const categoryPrivList: Priv = await privileges.categories.getPrivilegeList() as Priv;
+            const categoryPrivs = privs.filter(priv => categoryPrivList.includes(priv));
+            await privileges.categories[type](categoryPrivs, data.cid, data.member);
+        }
+
+        await events.log({
+            uid: caller.uid,
+            type: 'privilege-change',
+            ip: caller.ip,
+            privilege: data.privilege.toString(),
+            cid: data.cid,
+            action: data.set ? 'grant' : 'rescind',
+            target: data.member,
+        });
+    },
 };
+export = categoriesAPI;
