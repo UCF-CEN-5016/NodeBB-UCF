9865b04d432268836ba19b157d0d30b2446eb40a
Siri M <siri@Siris-Air.lan>
2024-01-30 23:15:25 -0500

    Added JS file

--- src/topics/suggested.js
+++ src/topics/suggested.js
@@ -11,50 +11,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = __importDefault(require("lodash"));
-const user_1 = __importDefault(require("../user"));
-const privileges_1 = __importDefault(require("../privileges"));
-const search_1 = __importDefault(require("../search"));
-function default_1(getTopicTags, getTopicsByTids, getTopicFields, getSortedSetRevRange, getSortedSetRevRangeByScore) {
-    const getSuggestedTopics = function (tid, uid, start, stop, cutoff = 0) {
+module.exports = function (Topics) {
+    Topics.getSuggestedTopics = function (tid, uid, start, stop, cutoff = 0) {
         return __awaiter(this, void 0, void 0, function* () {
             let tids;
             tid = parseInt(tid.toString(), 10);
-            cutoff = cutoff === 0 ? cutoff : (cutoff * 2592000000);
+            cutoff = cutoff === 0 ? cutoff : cutoff * 2592000000;
             const [tagTids, searchTids] = yield Promise.all([
-                getTidsWithSameTags(tid, cutoff),
-                getSearchTids(tid, uid, cutoff),
+                getTidsWithSameTags(tid, cutoff, Topics),
+                getSearchTids(tid, uid, cutoff, Topics),
             ]);
             tids = lodash_1.default.uniq(tagTids.concat(searchTids));
             let categoryTids = [];
             if (stop !== -1 && tids.length < stop - start + 1) {
-                categoryTids = yield getCategoryTids(tid, cutoff);
+                categoryTids = yield getCategoryTids(tid, cutoff, Topics);
             }
             tids = lodash_1.default.shuffle(lodash_1.default.uniq(tids.concat(categoryTids)));
-            tids = yield privileges_1.default.topics.filterTids('topics:read', tids, uid);
-            let topicData = yield getTopicsByTids(tids, uid);
+            tids = yield Topics.privileges.topics.filterTids('topics:read', tids, uid);
+            let topicData = yield Topics.getTopicsByTids(tids, uid);
             topicData = topicData.filter((topic) => topic && topic.tid !== tid);
-            topicData = yield user_1.default.blocks.filter(uid, topicData);
-            topicData = topicData.slice(start, stop !== -1 ? stop + 1 : undefined)
+            topicData = yield Topics.user.blocks.filter(uid, topicData);
+            topicData = topicData
+                .slice(start, stop !== -1 ? stop + 1 : undefined)
                 .sort((t1, t2) => t2.timestamp - t1.timestamp);
             return topicData;
         });
     };
-    function getTidsWithSameTags(tid, cutoff) {
+    function getTidsWithSameTags(tid, cutoff, Topics) {
         return __awaiter(this, void 0, void 0, function* () {
-            const tags = yield getTopicTags(tid);
-            let tids = cutoff === 0 ?
-                yield getSortedSetRevRange(tags.map((tag) => `tag:${tag}:topics`), 0, -1) :
-                yield getSortedSetRevRangeByScore(tags.map((tag) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
+            const tags = yield Topics.getTopicTags(tid);
+            let tids = cutoff === 0
+                ? yield Topics.db.getSortedSetRevRange(tags.map((tag) => `tag:${tag}:topics`), 0, -1)
+                : yield Topics.db.getSortedSetRevRangeByScore(tags.map((tag) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
             tids = tids.filter((_tid) => _tid !== tid); // remove self
             return lodash_1.default.shuffle(lodash_1.default.uniq(tids)).slice(0, 10).map(Number);
         });
     }
-    function getSearchTids(tid, uid, cutoff) {
+    function getSearchTids(tid, uid, cutoff, Topics) {
         return __awaiter(this, void 0, void 0, function* () {
-            const topicData = yield getTopicFields(tid, ['title', 'cid']);
-            const data = yield search_1.default.search({
+            const topicData = yield Topics.getTopicFields(tid, ['title', 'cid']);
+            const data = yield Topics.search.search({
                 query: topicData.title,
                 searchIn: 'titles',
                 matchWords: 'any',
@@ -68,17 +65,14 @@ function default_1(getTopicTags, getTopicsByTids, getTopicFields, getSortedSetRe
             return lodash_1.default.shuffle(data.tids).slice(0, 10).map(Number);
         });
     }
-    function getCategoryTids(tid, cutoff) {
+    function getCategoryTids(tid, cutoff, Topics) {
         return __awaiter(this, void 0, void 0, function* () {
-            const cid = yield getTopicFields(tid, 'cid');
-            const tids = cutoff === 0 ?
-                yield getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9) :
-                yield getSortedSetRevRangeByScore(`cid:${cid}:tids:lastposttime`, 0, 9, '+inf', Date.now() - cutoff);
-            return lodash_1.default.shuffle(tids.map(Number).filter(_tid => _tid !== tid));
+            const cid = yield Topics.getTopicField(tid, 'cid');
+            const tids = cutoff === 0
+                ? yield Topics.db.getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9)
+                : yield Topics.db.getSortedSetRevRangeByScore(`cid:${cid}:tids:lastposttime`, 0, 9, '+inf', Date.now() - cutoff);
+            return lodash_1.default.shuffle(tids.map(Number).filter((_tid) => _tid !== tid));
         });
     }
-    return {
-        getSuggestedTopics
-    };
-}
-exports.default = default_1;
+    ;
+};
--- src/topics/suggested.ts
+++ src/topics/suggested.ts
@@ -1,48 +1,62 @@
 import _ from 'lodash';
+
 import db from '../database';
 import user from '../user';
 import privileges from '../privileges';
 import search from '../search';
 
-export default function (getTopicTags: any, getTopicsByTids: any, getTopicFields: any, getSortedSetRevRange: any, getSortedSetRevRangeByScore: any) {
-    const getSuggestedTopics = async function (tid: number, uid: number, start: number, stop: number, cutoff: number = 0) {
+export = function (Topics) {
+    Topics.getSuggestedTopics = async function (
+        tid: number,
+        uid: number,
+        start: number,
+        stop: number,
+        cutoff: number = 0
+    ) {
         let tids: number[];
         tid = parseInt(tid.toString(), 10);
-        cutoff = cutoff === 0 ? cutoff : (cutoff * 2592000000);
+        cutoff = cutoff === 0 ? cutoff : cutoff * 2592000000;
         const [tagTids, searchTids] = await Promise.all([
-            getTidsWithSameTags(tid, cutoff),
-            getSearchTids(tid, uid, cutoff),
+            getTidsWithSameTags(tid, cutoff, Topics),
+            getSearchTids(tid, uid, cutoff, Topics),
         ]);
 
         tids = _.uniq(tagTids.concat(searchTids));
 
         let categoryTids: number[] = [];
         if (stop !== -1 && tids.length < stop - start + 1) {
-            categoryTids = await getCategoryTids(tid, cutoff);
+            categoryTids = await getCategoryTids(tid, cutoff, Topics);
         }
         tids = _.shuffle(_.uniq(tids.concat(categoryTids)));
-        tids = await privileges.topics.filterTids('topics:read', tids, uid);
+        tids = await Topics.privileges.topics.filterTids('topics:read', tids, uid);
 
-        let topicData = await getTopicsByTids(tids, uid);
+        let topicData = await Topics.getTopicsByTids(tids, uid);
         topicData = topicData.filter((topic: any) => topic && topic.tid !== tid);
-        topicData = await user.blocks.filter(uid, topicData);
-        topicData = topicData.slice(start, stop !== -1 ? stop + 1 : undefined)
+        topicData = await Topics.user.blocks.filter(uid, topicData);
+        topicData = topicData
+            .slice(start, stop !== -1 ? stop + 1 : undefined)
             .sort((t1: any, t2: any) => t2.timestamp - t1.timestamp);
         return topicData;
     };
 
-    async function getTidsWithSameTags(tid: number, cutoff: number) {
-        const tags = await getTopicTags(tid);
-        let tids = cutoff === 0 ?
-            await getSortedSetRevRange(tags.map((tag: string) => `tag:${tag}:topics`), 0, -1) :
-            await getSortedSetRevRangeByScore(tags.map((tag: string) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
+    async function getTidsWithSameTags(tid: number, cutoff: number, Topics) {
+        const tags = await Topics.getTopicTags(tid);
+        let tids = cutoff === 0
+            ? await Topics.db.getSortedSetRevRange(tags.map((tag: string) => `tag:${tag}:topics`), 0, -1)
+            : await Topics.db.getSortedSetRevRangeByScore(
+                  tags.map((tag: string) => `tag:${tag}:topics`),
+                  0,
+                  -1,
+                  '+inf',
+                  Date.now() - cutoff
+              );
         tids = tids.filter((_tid: number) => _tid !== tid); // remove self
         return _.shuffle(_.uniq(tids)).slice(0, 10).map(Number);
     }
 
-    async function getSearchTids(tid: number, uid: number, cutoff: number) {
-        const topicData = await getTopicFields(tid, ['title', 'cid']);
-        const data = await search.search({
+    async function getSearchTids(tid: number, uid: number, cutoff: number, Topics) {
+        const topicData = await Topics.getTopicFields(tid, ['title', 'cid']);
+        const data = await Topics.search.search({
             query: topicData.title,
             searchIn: 'titles',
             matchWords: 'any',
@@ -56,15 +70,17 @@ export default function (getTopicTags: any, getTopicsByTids: any, getTopicFields
         return _.shuffle(data.tids).slice(0, 10).map(Number);
     }
 
-    async function getCategoryTids(tid: number, cutoff: number) {
-        const cid = await getTopicFields(tid, 'cid');
-        const tids = cutoff === 0 ?
-            await getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9) :
-            await getSortedSetRevRangeByScore(`cid:${cid}:tids:lastposttime`, 0, 9, '+inf', Date.now() - cutoff);
-        return _.shuffle(tids.map(Number).filter(_tid => _tid !== tid));
-    }
-
-    return {
-        getSuggestedTopics
+    async function getCategoryTids(tid: number, cutoff: number, Topics) {
+        const cid = await Topics.getTopicField(tid, 'cid');
+        const tids = cutoff === 0
+            ? await Topics.db.getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9)
+            : await Topics.db.getSortedSetRevRangeByScore(
+                  `cid:${cid}:tids:lastposttime`,
+                  0,
+                  9,
+                  '+inf',
+                  Date.now() - cutoff
+              );
+        return _.shuffle(tids.map(Number).filter((_tid: number) => _tid !== tid));
     };
-}
+};
