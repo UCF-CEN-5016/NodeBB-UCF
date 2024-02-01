37a9d1172153d5e04b62628b40385b7353fa0b3a
Greesh12 <greeshwanthp@gmail.com>
2024-02-01 18:54:02 -0500

    Revert "Chnaged Import statements to work in typeScript. Added types to message, callback parameter in the forkChild function and then return message as Serializable type from child_process. Added type to rounds and password parameters in the hash function. Cast exports into right format before hasing within getFakeHash function. Added type defintions to password, hashm shaWrapped in the compare function. Wrote the type definition of method, and msg for the tryMethod function. Added type defintions to the parameterL type, rounds, password in hashPassword. Added type definition for msg parameter in compare fucntion and added a Promise Type. Added type definition for msg parameter in the child process. Also added then and catch calls to tryMethod to handle errors."
    This reverts commit 8bfd79b5164bda50bf7dcbc19b4c0a836559b768.

--- src/password.ts
+++ src/password.ts
@@ -1,112 +1,80 @@
-// The next few line calls a function in a module that has not been updated to TS yet
-// eslint-disable-next-line import/no-import-module-exports
+'use strict';
+
 import * as path from 'path';
-// eslint-disable-next-line import/no-import-module-exports
-import { createHash } from 'crypto';
-// eslint-disable-next-line import/no-import-module-exports
+import * as crypto from 'crypto';
 import * as util from 'util';
+import * as bcrypt from 'bcryptjs';
 
-// eslint-disable-next-line import/no-import-module-exports
-import bcrypt from 'bcryptjs';
+import { fork } from './meta/debugFork';
 
-// eslint-disable-next-line import/no-import-module-exports
-import { Serializable } from 'child_process';
-
-// eslint-disable-next-line import/no-import-module-exports
-import fork from './meta/debugFork';
-
-// eslint-disable-next-line import/no-import-module-exports
-
-// The next line calls a function in a module that has not been updated to TS yet
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function forkChild(message: any, callback: (error: Error | null, result?: string) => void) {
+function forkChild(message:any, callback:(error:Error | null, result ?: any) => void) {
     const child = fork(path.join(__dirname, 'password'));
 
-    child.on('message', (msg:{ err?: string; result?: string }) => {
+    child.on('message', (msg) => {
         callback(msg.err ? new Error(msg.err) : null, msg.result);
     });
-    child.on('error', (err:Error) => {
+    child.on('error', (err) => {
         console.error(err.stack);
         callback(err);
     });
 
-    child.send(message as Serializable);
+    child.send(message);
 }
 
 const forkChildAsync = util.promisify(forkChild);
-// The next line calls a function in a module that has not been updated to TS yet
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+
 exports.hash = async function (rounds:number, password:string) {
-    password = createHash('sha512').update(password).digest('hex');
-    return await forkChildAsync({ type: 'hash', rounds: rounds, password: password });
+    password = crypto.createHash('sha512').update(password).digest('hex');
+    return await forkChildAsync({ type: 'hash', rounds, password});
 };
 
-let fakeHashCache: string | undefined;
+exports.compare = async function (password:string, hash:string, shaWrapped:boolean) {
+    const fakeHash = await getFakeHash();
+
+    if (shaWrapped) {
+        password = crypto.createHash('sha512').update(password).digest('hex');
+    }
+
+    return await forkChildAsync({ type: 'compare', password, hash: hash || fakeHash });
+};
+
+let fakeHashCache : string | undefined;
 async function getFakeHash() {
     if (fakeHashCache) {
         return fakeHashCache;
     }
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    fakeHashCache = await (exports as { hash: (arg1: number, arg2: string) => Promise<string> }).hash(12,
-        Math.random().toString());
+    fakeHashCache = await exports.hash(12, Math.random().toString());
     return fakeHashCache;
 }
 
-// The next line calls a function in a module that has not been updated to TS yet
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-exports.compare = async function (password:string, hash:string, shaWrapped:boolean) {
-    const fakeHash:string = await getFakeHash();
-
-    if (shaWrapped) {
-        password = createHash('sha512').update(password).digest('hex');
+// child process
+process.on('message', (msg:string) => {
+    if (msg === 'hash') {
+        tryMethod(hashPassword, msg);
+    } else if (msg === 'compare') {
+        tryMethod(compare, msg);
     }
+});
 
-    return await forkChildAsync({ type: 'compare', password: password, hash: hash || fakeHash });
-};
-
-async function tryMethod(
-    method: (msg: { type: string, rounds: number, password: string, hash?: string }) => Promise<unknown>,
-    msg: { type: string, rounds: number, password: string, hash?: string }
-) {
+async function tryMethod(method:(msg:any) => Promise<any>, msg:any) {
     try {
         const result = await method(msg);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        process.send({ result });
+        process.send({ result: result });
     } catch (err) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        process.send(err);
+        process.send({ err: err.message });
     } finally {
         process.disconnect();
     }
 }
 
-async function hashPassword(msg: { type: string, rounds: number, password: string }) {
-    // eslint-disable-next-line max-len
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
-    const salt: string = await bcrypt.genSalt(parseInt(String(msg.rounds), 10));
-    // eslint-disable-next-line max-len
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
-    const hash: string = await bcrypt.hash(msg.password, salt);
+async function hashPassword(msg:any) {
+    const salt = await bcrypt.genSalt(parseInt(msg.rounds, 10));
+    const hash = await bcrypt.hash(msg.password, salt);
     return hash;
 }
 
-async function compare(msg: { type: string; rounds: number; password: string; hash?: string }): Promise<boolean> {
-    // eslint-disable-next-line max-len
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+async function compare(msg:any) {
     return await bcrypt.compare(String(msg.password || ''), String(msg.hash || ''));
 }
 
-// child process
-process.on('message', (msg: { type: string, rounds: number, password: string, hash?: string }) => {
-    if (msg.type === 'hash') {
-        tryMethod(hashPassword, msg).then().catch((error) => {
-            console.error(error);
-        });
-    } else if (msg.type === 'compare') {
-        tryMethod(compare, msg).then().catch((error) => {
-            console.error(error);
-        });
-    }
-});
-
+require('./promisify')(exports);
