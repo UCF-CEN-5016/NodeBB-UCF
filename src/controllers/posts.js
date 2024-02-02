4772e9c174869463346c5146bf4e6c309d3f72fe
MunaimahK <munim2k@gmail.com>
2024-02-01 22:01:50 -0500

    convert to js to ts

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,57 +32,56 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// @eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-cal
-// const querystring = require('querystring');
-// const posts = require('../posts');
-// const privileges = require('../privileges');
-// const helpers = require('./helpers');
-const querystring = require("querystring");
-const posts = require("../posts");
-const privileges = require("../privileges");
-const helpers = require("./helpers");
-const postsController = module.exports;
+/*
+import querystring = require('querystring');
+import posts = require('../posts');
+import privileges = require('../privileges');
+import helpers = require('./helpers');
+*/
+const querystring = __importStar(require("querystring"));
+const posts = __importStar(require("../posts"));
+const privileges = __importStar(require("../privileges"));
+const helpers = __importStar(require("./helpers"));
+// const postsController : postsController = module.exports as postsController;
 // postsController.redirectToPost = async function (req, res, next) {
-postsController.redirectToPost = function (req, res, next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const pid = parseInt(req.params.pid, 10);
-        if (!pid) {
-            return next();
-        }
-        const [canRead, path] = yield Promise.all([
-            privileges.posts.can('topics:read', pid, req.uid),
-            posts.generatePostPath(pid, req.uid),
-        ]);
-        if (!path) {
-            return next();
-        }
-        if (!canRead) {
-            return helpers.notAllowed(req, res);
-        }
-        const qs = querystring.stringify(req.query);
-        helpers.redirect(res, qs ? `${path}?${qs}` : path);
-    });
-};
-postsController.getRecentPosts = function (req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const page = parseInt(req.query.page, 10) || 1;
-        const postsPerPage = 20;
-        const start = Math.max(0, (page - 1) * postsPerPage);
-        const stop = start + postsPerPage - 1;
-        /*
-        const data = await posts.getRecentPosts(req.uid, start, stop, req.params.term);
-        res.json(); */
-        try {
-            const data = yield posts.getRecentPosts(req.uid, start, stop, req.params.term);
-            //  res.json(data);
-            //  res.json(data as Promise<void>);
-            console.log(data);
-        }
-        catch (error) {
-            // Handle errors appropriately
-            console.error('Error:', error);
-            // res.status(500).json({ error: 'Internal Server Error' });
-        }
-        // res.json(data)
-    });
-};
+function default_1(postsController) {
+    postsController.redirectToPost = function (req, res, next) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const pid = parseInt(req.params.pid, 10);
+            if (!pid) {
+                return next();
+            }
+            const [canRead, path] = yield Promise.all([
+                privileges.posts.can('topics:read', pid, req.uid),
+                posts.generatePostPath(pid, req.uid),
+            ]);
+            if (!path) {
+                return next();
+            }
+            if (!canRead) {
+                return helpers.notAllowed(req, res);
+            }
+            const qs = querystring.stringify(req.query);
+            helpers.redirect(res, qs ? `${path}?${qs}` : path);
+        });
+    };
+    postsController.getRecentPosts = function (req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const page = parseInt(req.query.page, 10) || 1;
+            const postsPerPage = 20;
+            const start = Math.max(0, (page - 1) * postsPerPage);
+            const stop = start + postsPerPage - 1;
+            try {
+                const data = yield posts.getRecentPosts(req.uid, start, stop, req.params.term);
+                res.json(data);
+            }
+            catch (error) {
+                // Handle errors appropriately
+                console.error('Error:', error);
+                // res.status(500).json({ error: 'Internal Server Error' });
+            }
+            // res.json(data)
+        });
+    };
+}
+exports.default = default_1;
--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -3,10 +3,18 @@
 // const posts = require('../posts');
 // const privileges = require('../privileges');
 // const helpers = require('./helpers');
+import { Response } from 'express';
+/*
 import querystring = require('querystring');
 import posts = require('../posts');
 import privileges = require('../privileges');
 import helpers = require('./helpers');
+*/
+
+import * as querystring from 'querystring';
+import * as posts from '../posts';
+import * as privileges from '../privileges';
+import * as helpers from './helpers';
 
 interface CustomRequest extends Request {
     params?: {
@@ -22,50 +30,47 @@ type postsController = {
     getRecentPosts: (req: CustomRequest, res: Response) => Promise<void>;
     redirectToPost: (req: CustomRequest, res: Response, next) => Promise<void>;
 }
-const postsController : postsController = module.exports as postsController;
+// const postsController : postsController = module.exports as postsController;
 
 
 // postsController.redirectToPost = async function (req, res, next) {
-postsController.redirectToPost = async function (req: CustomRequest, res: Response, next): Promise<void> {
-    const pid = parseInt(req.params.pid, 10);
-    if (!pid) {
-        return next() as void;
-    }
-    type CanReadResult = boolean;
-    type PostPathResult = string;
-    const [canRead, path]: [CanReadResult, PostPathResult] = await Promise.all([
-        privileges.posts.can('topics:read', pid, req.uid),
-        posts.generatePostPath(pid, req.uid),
-    ]) as [CanReadResult, PostPathResult];
-    if (!path) {
-        return next() as void;
-    }
-    if (!canRead) {
-        return helpers.notAllowed(req, res) as void;
-    }
+export default function (postsController: postsController) {
+    postsController.redirectToPost = async function (req: CustomRequest, res: Response, next): Promise<void> {
+        const pid = parseInt(req.params.pid, 10);
+        if (!pid) {
+            return (next() as void);
+        }
+        type CanReadResult = boolean;
+        type PostPathResult = string;
+        const [canRead, path]: [CanReadResult, PostPathResult] = await Promise.all([
+            privileges.posts.can('topics:read', pid, req.uid),
+            posts.generatePostPath(pid, req.uid),
+        ]) as [CanReadResult, PostPathResult];
+        if (!path) {
+            return next() as void;
+        }
+        if (!canRead) {
+            return helpers.notAllowed(req, res) as void;
+        }
 
-    const qs = querystring.stringify(req.query);
-    helpers.redirect(res, qs ? `${path}?${qs}` : path);
-};
+        const qs = querystring.stringify(req.query);
+        helpers.redirect(res, qs ? `${path}?${qs}` : path);
+    };
 
-postsController.getRecentPosts = async function (req: CustomRequest, res: Response): Promise<void> {
-    const page = parseInt(req.query.page, 10) || 1;
-    const postsPerPage = 20;
-    const start = Math.max(0, (page - 1) * postsPerPage);
-    const stop = start + postsPerPage - 1;
-    /*
-    const data = await posts.getRecentPosts(req.uid, start, stop, req.params.term);
-    res.json(); */
+    postsController.getRecentPosts = async function (req: CustomRequest, res: Response) {
+        const page = parseInt(req.query.page, 10) || 1;
+        const postsPerPage = 20;
+        const start = Math.max(0, (page - 1) * postsPerPage);
+        const stop = start + postsPerPage - 1;
 
-    try {
-        const data:Promise<void> = await posts.getRecentPosts(req.uid, start, stop, req.params.term);
-        //  res.json(data);
-        //  res.json(data as Promise<void>);
-        console.log(data);
-    } catch (error) {
-        // Handle errors appropriately
-        console.error('Error:', error);
-        // res.status(500).json({ error: 'Internal Server Error' });
-    }
-    // res.json(data)
-};
+        try {
+            const data = await posts.getRecentPosts(req.uid, start, stop, req.params.term);
+            res.json(data);
+        } catch (error) {
+            // Handle errors appropriately
+            console.error('Error:', error);
+            // res.status(500).json({ error: 'Internal Server Error' });
+        }
+        // res.json(data)
+    };
+}
